2cfa872c60cfc7db2090dd842ee80d4e034b1392
hauphanvan <48856533+hauphanvan@users.noreply.github.com>
2019-04-22 00:29:22 +0700

    update

--- src/main/java/com/pack/hauphanvan/config/DB.java
+++ src/main/java/com/pack/hauphanvan/config/DB.java
@@ -2,7 +2,7 @@ package com.pack.hauphanvan.config;
 
 public interface DB {
 	String driverClass="com.mysql.cj.jdbc.Driver";
-	String urlVar = "jdbc:mysql://us-cdbr-iron-east-02.cleardb.net:3306/heroku_e1e1fff7a223984";
+	String urlVar = "jdbc:mysql://b19019bd8539f9:09fa5f98@us-cdbr-iron-east-02.cleardb.net:3306/heroku_e1e1fff7a223984";
 	String userName ="b19019bd8539f9";
 	String password = "09fa5f98";
 }
