5e7a74e4de800deb07f27a5901e309d15642f17f
hauphanvan <48856533+hauphanvan@users.noreply.github.com>
2019-04-22 00:23:28 +0700

    update

--- src/main/java/com/pack/hauphanvan/config/DB.java
+++ src/main/java/com/pack/hauphanvan/config/DB.java
@@ -2,7 +2,7 @@ package com.pack.hauphanvan.config;
 
 public interface DB {
 	String driverClass="com.mysql.cj.jdbc.Driver";
-	String urlVar = "us-cdbr-iron-east-02.cleardb.net:";
+	String urlVar = "us-cdbr-iron-east-02.cleardb.net";
 	String userName ="b19019bd8539f9";
 	String password = "09fa5f98";
 }
