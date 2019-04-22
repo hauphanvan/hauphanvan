16424308301f29587de64b5176f3877f472d177f
Nguyen Tai Linh <tienkhongtoquoc@gmail.com>
2019-04-22 13:18:52 +0700

    athorized config update

--- src/main/java/com/pack/hauphanvan/config/SecurityConfig.java
+++ src/main/java/com/pack/hauphanvan/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.formLogin().loginPage("/login").usernameParameter("username").passwordParameter("password");
-		http.formLogin().defaultSuccessUrl("/product/products").failureUrl("/login?errors");
+		http.formLogin().defaultSuccessUrl("/product/").failureUrl("/login?errors");
 		http.logout().logoutSuccessUrl("/login?logout");
 		http.exceptionHandling().accessDeniedPage("/login?accessDenied");
 		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/**/add/").access("hasRole('ADMIN')").antMatchers("/**/products").access("hasRole('USER')");
