b0c78b00b1cc1a1769d6712327ce4b9f2d87be11
Nguyen Tai Linh <tienkhongtoquoc@gmail.com>
2019-04-28 12:01:46 +0700

    update welcom

--- src/main/java/com/pack/hauphanvan/controller/HomeController.java
+++ src/main/java/com/pack/hauphanvan/controller/HomeController.java
@@ -11,8 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 	@RequestMapping
 	public String welcome(Model model, HttpServletRequest request) {
-		model.addAttribute("greeting", "Welcome to HauPhanVan.com");
-		model.addAttribute("tagline", "The one and only awsome website in the world");
+		model.addAttribute("greeting", "Welcome to HauPhanVan");
+		model.addAttribute("tagline", "This is my first website, and it is not complete yet. But someday it will. "
+				+ "And for temporary this website is based on spring mvc, spring security for login, "
+				+ "jdbcTemplate for connect to database, and database use in this website is mysql clearDB and free DB host, "
+				+ "jsp for view part, a little bootstrap for decoration, and it running on heroku free host server. "
+				+ "It look very simple, and nothing much yet, i know. But someday it will become a very very amazing website. "
+				+ "And if you are reading this right now, you know where did the most amazing website look like at the beginning :))");
 		System.out.println("hello");
 		System.out.println((String)request.getServletContext().getRealPath("/"));
 		return "welcome";
